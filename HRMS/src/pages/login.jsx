784520d61ebf0e5c90b4709dfe303767ae0f8caa
Pranjal-127 <pranjalbhattarai247@gmail.com>
2025-07-14 09:53:21 +0545

    Protected Route Complete

--- HRMS/src/pages/login.jsx
+++ HRMS/src/pages/login.jsx
@@ -1,8 +1,9 @@
-
-   import {useState } from "react"
-export default function Loginform() {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+export default function LoginForm(){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -12,53 +13,49 @@ export default function Loginform() {
         }
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if(!emailRegex.test(email)) {
-            alert("Please enter a valid email address");
+            alert ("Please enter a valid email address");
             return;
         }
-        if (email !== "eve.holt@reqres.in" || password !== "cityslicka") {
-      alert(
-        "For demo login, use:\nEmail: eve.holt@reqres.in\nPassword: cityslicka"
-      );
-
-      return;
-    }
-    fetch("https://reqres.in/api/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "x-api-key": "reqres-free-v1",
-        },
-        body: JSON.stringify({ email, password}),
-    })
-    .then((resp) => {
-        if (!resp) {
-            throw new Error("LoginFailed");
-        } else {
-            return resp.json();
+         if (email !== "eve.holt@reqres.in" || password !== "cityslicka") {
+            alert(
+                "For demo login, use:\nEmail: eve.holt@reqres.in\nPassword: cityslicka");
+            return;
         }
-            
-        })
-        .then((data) => {
-            localStorage.setItem("token", data.token);
-            alert("Login Succed");
-            navigate ("/home");
-            setEmail("");
-            setPassword("");
-        })
-        .catch((error) => {
-            console.log("Login Error:", error);
-            alert(error.message);
-        });
-    };
-
-    return (
+        fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "reqres-free-v1",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((resp) => {
+        if (!resp) {
+          throw new Error("Login Failed");
+        } else {
+          return resp.json();
+        }
+      })
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+        alert("Login Succeed");
+        navigate("/home");
+        setEmail("");
+        setPassword("");
+      })
+      .catch((error) => {
+        console.log("Login Error:", error);
+        alert(error.message);
+      });
         
- <div class="flex items-center justify-center min-h-screen bg-gray-100 px-4">
-      <form class="bg-white p-8 rounded-xl shadow-md w-full max-w-sm"
-        onSubmit={handleSubmit}
-        >
-        <div class="flex justify-center mb-4 text-black-600 text-4xl">
-          <span>👤</span>
+    }
+    return(
+         <div class="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+            <form 
+                class="bg-white p-8 rounded-xl shadow-md w-full max-w-sm"
+                onSubmit = {handleSubmit} >
+            <div class="flex justify-center mb-4 text-black-600 text-4xl">
+            <span>👤</span>
         </div>
         <h2 class="text-2xl font-bold mb-6 text-center text-black-700">
           HRMS Login
@@ -89,6 +86,6 @@ export default function Loginform() {
           Login
         </button>
       </form>
-    </div> 
+    </div>
     )
-    }
+}
